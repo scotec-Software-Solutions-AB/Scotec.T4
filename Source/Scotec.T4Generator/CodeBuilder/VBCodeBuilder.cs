993c5e0a7416afa9cbb94c66eb1e687401682280
Olaf Meyer <olaf.meyer@e-ypsilon.de>
2022-11-17 03:37:35 +0100

    Added templates

--- Source/Scotec.T4Generator/CodeBuilder/VBCodeBuilder.cs
+++ Source/Scotec.T4Generator/CodeBuilder/VBCodeBuilder.cs
@@ -3,7 +3,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Scotec.T4Generator.Resources;
 using Scotec.T4Generator.Syntax;
 
 #endregion
---
+++ Source/Scotec.T4Generator/Templates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scotec.T4Generator
+{
+    internal class Templates
+    {
+        public static string CSCodeTemplate => ReadResource();
+        public static string CSIncludeTemplate => ReadResource();
+        public static string VBCodeTemplate => ReadResource();
+        public static string VBIncludeTemplate => ReadResource();
+
+        private static string ReadResource([CallerMemberName]string name = null)
+        {
+            // Determine path
+            var assembly = Assembly.GetExecutingAssembly();
+            // Format: "{Namespace}.{Folder}.{filename}.{Extension}"
+            var resourcePath = 
+                assembly.GetManifestResourceNames()
+                .Single(str => str.Contains(name));
+
+                using Stream stream = assembly.GetManifestResourceStream(resourcePath);
+                using StreamReader reader = new StreamReader(stream);
+                return reader.ReadToEnd();
+        }
+    }
+}
